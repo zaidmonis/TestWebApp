0bd53008acce0ecdea03a5925c22101281c8b2df
Zaid <mzaid@onecallcm.com>
2022-02-01 16:44:44 +0530

    Add Stylecop linter

--- TestWebApp/Controllers/ValueController.cs
+++ TestWebApp/Controllers/ValueController.cs
@@ -1,17 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
-
 namespace TestWebApp.Controllers
 {
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+
     [Route("api/[controller]")]
     [ApiController]
     public class ValueController : ControllerBase
     {
         [HttpGet]
-        public ActionResult<IEnumerable<string>> getValues()
+        public ActionResult<IEnumerable<string>> GetValues()
         {
-            return new string[] {"a", "b"};
+            return new[] { "a", "b" };
         }
     }
 }
